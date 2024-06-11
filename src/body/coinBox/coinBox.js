99d96c55d1d78d3f52d446d758ec5de107fb6123
Brian Del Carpio <bda59@sfu.ca>
2024-06-11 00:02:34 -0700

    usestate

--- src/body/coinBox/coinBox.js
+++ src/body/coinBox/coinBox.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ClipboardJS from 'clipboard';
 import { copyImage, twitterImage, telegramImage, websiteImage, pumpFunImage, imgNotFound } from './links';
 import './coinBox.css';
